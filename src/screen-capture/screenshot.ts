d8cea43cdea7bbdfded9f77fea6f3950b3b9c189
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-16 14:19:29 +0800

    [feat] switch to playwright

--- src/screen-capture/screenshot.ts
+++ src/screen-capture/screenshot.ts
@@ -1,14 +1,15 @@
 import { chromium, devices } from 'playwright';
-import uint8ArrayToString from '../uint8ArrayToString.js';
+import uint8ArrayToString from '../utils/uint8ArrayToString.js';
 import sizeOf from 'buffer-image-size';
+import isValidSettings from '../utils/verifySettings.js';
 
 interface Params {
   URL: string;
   userId: string;
-  settings: Setting;
+  settings: Settings;
 }
 
-interface Setting {
+interface Settings {
   width: number;
   height: number;
   emulateDevice: string;
@@ -16,24 +17,51 @@ interface Setting {
 }
 
 export default async function snapOne(params: Params) {
-  const browser = await chromium.launch();
+  try {
+    console.log('\nuser ' + params.userId + ' is using the service, with settings: ');
+    console.log(params.settings);
 
-  const iphone13 = devices['iPhone 13'];
-  console.log(iphone13);
-  const context = await browser.newContext({
-    ...iphone13,
-  });
+    if (!isValidSettings(params.settings)) {
+      console.log('\ninvalid settings: ');
+      console.log(params.settings);
+      return { msg: 'invalid settings' };
+    }
 
-  // Create context with given viewport
-  // const context = await browser.newContext({
-  //   viewport: { width: 1280, height: 1024 }
-  // });
-  const page = await context.newPage();
-  await page.goto(params.URL);
-  const img_unit8Arr = await page.screenshot({ fullPage: false });
+    const browser = await chromium.launch();
+    const device = params.settings.emulateDevice ? devices[params.settings.emulateDevice] : null;
+    const deviceScreen = device ? JSON.parse(JSON.stringify(device)).screen : null;
 
-  //check if buffer is Uint8Array
-  if (img_unit8Arr instanceof Uint8Array) {
+    const viewport = device
+      ? deviceScreen
+        ? params.settings.emulateDevice.includes('landscape')
+          ? {
+              width: deviceScreen.width > deviceScreen.height ? deviceScreen.width * 1 : deviceScreen.height * 1,
+              height: deviceScreen.width > deviceScreen.height ? deviceScreen.height * 1 : deviceScreen.width * 1,
+            }
+          : deviceScreen
+        : device.viewport
+      : // ? device.viewport
+        {
+          width: params.settings.width * 1,
+          height: params.settings.height * 1,
+        };
+
+    // console.log('\ndevice: ');
+    // console.log(device);
+    // console.log('\nviewport: ');
+    // console.log(viewport);
+
+    const context = await browser.newContext({
+      ...device,
+      viewport,
+      deviceScaleFactor: 1,
+    });
+    const page = await context.newPage();
+    await page.goto(params.URL);
+    const img_unit8Arr = await page.screenshot({ timeout: 0, fullPage: false });
+
+    // check if buffer is Uint8Array
+    // console.log(img_unit8Arr instanceof Uint8Array);
     const dim = sizeOf(img_unit8Arr);
     const screen = params.settings.emulateDevice ? params.settings.emulateDevice : `${dim.width}x${dim.height}`;
 
@@ -43,12 +71,13 @@ export default async function snapOne(params: Params) {
       height: dim.height,
       snap_data: uint8ArrayToString(img_unit8Arr),
     };
+  } catch (e) {
+    console.log('screenshot failed: ');
+    console.log(params);
+    console.log(e);
+    return {
+      msg: 'screenshot failed',
+      error: e,
+    };
   }
-  //   console.log(buffer.toString('base64'));
-  //   return {
-  //     name: 'screenshot',
-  //     width: 1280,
-  //     height: 1024,
-  //     snap_data: buffer.toString('base64'),
-  //   }
 }
