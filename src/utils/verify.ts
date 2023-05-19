357ce0138bd4a4548d7a68b402251890c5439b2a
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-19 12:45:42 +0800

    [bugfix] verify check if userId exist

--- src/utils/verify.ts
+++ src/utils/verify.ts
@@ -1,7 +1,8 @@
 import { Request } from 'express';
 
 export default function verify(req: Request) {
-  return req.headers['user-agent']?.includes('Figma')
+  // return req.headers['user-agent']?.includes('Figma')
+  return !!req.body.userId;
 //   const rule = (element: String) => element.toLowerCase().includes('figma');
 //   return req.rawHeaders.some(rule);
 }
