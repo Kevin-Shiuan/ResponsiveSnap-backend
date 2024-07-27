55b1af0d59b2fa860090dfc6a47f85df2a91e806
Kevin-Shiuan <kevinlee@phase.com>
2024-07-27 16:19:28 +0800

    refactor: update checkDeviceSettings

--- src/utils/checkDeviceSettings.ts
+++ src/utils/checkDeviceSettings.ts
@@ -1,45 +1,30 @@
 import { DeviceSettings } from '../../types'
+import { ALL_DEVICES } from '../constants/devices'
 
-export default function checkDeviceSettings(settings: DeviceSettings) {
-  if (settings.emulateDevice === '') {
-    return isValidScreenSize(settings)
+const MAX_WIDTH = 4096
+const MIN_WIDTH = 360
+const MAX_HEIGHT = 4096
+const MIN_HEIGHT = 360
+
+export default function checkDeviceSettings({ width, height, emulateDevice, fullPage }: DeviceSettings) {
+  if (emulateDevice === 'custom') {
+    return isValidScreenSize({ width, height, fullPage })
   }
-  return isValidDevice(settings)
+  return isValidDevice(emulateDevice)
 }
 
-const isValidScreenSize = (settings: DeviceSettings) => {
-  return settings.height >= 360 || settings.width >= 360
+const isValidScreenSize = ({ width, height, fullPage }: Omit<DeviceSettings, 'emulateDevice'>) => {
+  const isValidWidth = MIN_WIDTH <= width && width <= MAX_WIDTH
+
+  if (fullPage) {
+    const isValidHeight = MIN_HEIGHT <= height
+    return isValidWidth && isValidHeight
+  }
+
+  const isValidHeight = MIN_HEIGHT <= height && height <= MAX_HEIGHT
+  return isValidWidth && isValidHeight
 }
 
-const isValidDevice = (settings: DeviceSettings) => {
-  return knowDevices.includes(settings.emulateDevice)
+const isValidDevice = (emulateDevice: DeviceSettings['emulateDevice']) => {
+  return ALL_DEVICES.includes(emulateDevice)
 }
-
-const knowDevices = [
-  'iPad (gen 7)',
-  'iPad (gen 7) landscape',
-  'iPad Mini',
-  'iPad Mini landscape',
-  'iPad Pro 11',
-  'iPad Pro 11 landscape',
-  'iPhone SE',
-  'iPhone SE landscape',
-  'iPhone 12',
-  'iPhone 12 landscape',
-  'iPhone 12 Pro',
-  'iPhone 12 Pro landscape',
-  'iPhone 12 Pro Max',
-  'iPhone 12 Pro Max landscape',
-  'iPhone 12 Mini',
-  'iPhone 12 Mini landscape',
-  'iPhone 13',
-  'iPhone 13 landscape',
-  'iPhone 13 Pro',
-  'iPhone 13 Pro landscape',
-  'iPhone 13 Pro Max',
-  'iPhone 13 Pro Max landscape',
-  'iPhone 13 Mini',
-  'iPhone 13 Mini landscape',
-  'Pixel 5',
-  'Pixel 5 landscape'
-]
