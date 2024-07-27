c55c664ee52fafb74f12cd5d590f71e8decd08aa
Kevin-Shiuan <kevinlee@phase.com>
2024-07-27 15:29:10 +0800

    refactor: remove unused code

--- src/utils/verify.ts
+++ src/utils/verify.ts
@@ -1,8 +1,5 @@
 import { Request } from 'express';
 
 export default function verify(req: Request) {
-  // return req.headers['user-agent']?.includes('Figma')
   return !!req.body.userId;
-//   const rule = (element: String) => element.toLowerCase().includes('figma');
-//   return req.rawHeaders.some(rule);
 }
